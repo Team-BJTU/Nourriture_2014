3ce07c5105dd3dc27dcef962c8410e6a559791db
roy_m <roy_m@epitech.eu>
2014-11-13 15:29:29 +0800

    MAJ Basic files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express')
+/*var express = require('express')
   , app = express.createServer()
 
 app.get('/', function(req, res){
@@ -9,3 +9,15 @@ app.get('/', function(req, res){
 
 app.listen(8080);
 console.log('Express app started on port 8080');
+*/
+
+var express = require('express');
+var express = express();
+
+app.get('/', function(req, res){
+	res.send('Hello world')
+});
+
+app.listen(process.env.PORT || 5000);
+
+module.exports = app;

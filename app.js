dd372c1d9c0534ea5bea998a03179991ca90cc5c
Ubuntu <ubuntu@ip-172-31-26-74.ap-northeast-1.compute.internal>
2014-11-16 05:36:36 +0000

    Modify app.js to have a server

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express     = require('express')
-  , db          = require('./db')
+  //, db          = require('./db')
   , bodyParser  = require('body-parser');
 var app         = express()
   , port        = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(require('./controllers'));
+//app.use(require('./controllers'));
 app.listen(port);
 console.log('Express server listening on port ' + port);

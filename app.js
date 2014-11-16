9fb7c5cfe1a3a10548011017c37bf418304da2a5
Ubuntu <ubuntu@ip-172-31-26-74.ap-northeast-1.compute.internal>
2014-11-16 04:52:17 +0000

    add modules

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-/*var express = require('express')
-  , app = express.createServer()
-
-app.get('/', function(req, res){
-  res.send([
-      '<h1>Hello World</h1>'
-  ].join('\n'));
-});
-
-app.listen(8080);
-console.log('Express app started on port 8080');
-*/
-
-var express = require('express');
-var express = express();
-
-app.get('/', function(req, res){
-	res.send('Hello world')
-});
-
-app.listen(process.env.PORT || 5000);
-
-module.exports = app;
+var express     = require('express')
+  , db          = require('./db')
+  , bodyParser  = require('body-parser');
+var app         = express()
+  , port        = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(require('./controllers'));
+app.listen(port);
+console.log('Express server listening on port ' + port);

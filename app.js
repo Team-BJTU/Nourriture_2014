12703901c5e206f6b811d39827be43467d0b38e6
anteno_y <anteno_y@epitech.eu>
2014-11-30 01:43:45 +0800

    - initial commit 	- Fonctionnal server 	- initial DB

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+    ingredient = require('./routes/ingredients');
+
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser'); 
+var app = express();
+ 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+ 
+app.get('/ingredients', ingredient.findAll);
+app.get('/ingredients/:id', ingredient.findById);
+app.post('/ingredients', ingredient.addIngredient);
+app.put('/ingredients/:id', ingredient.updateIngredient);
+app.delete('/ingredients/:id', ingredient.deleteIngredient);
+ 
+app.listen(3000);
+console.log('Listening on port 3000...');
